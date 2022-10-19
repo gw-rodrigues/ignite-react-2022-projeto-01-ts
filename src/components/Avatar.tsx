bc04c343e6a5a34f9bd38959c7260bdb76b4804c
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-10-19 17:42:42 +0200

    feat: add interface Avatar. add alt prop

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,10 +1,17 @@
-import styles from "./Avatar.module.css";
+import styles from './Avatar.module.css';
 
-export function Avatar({ src, hasBorder = true }) {
+interface AvatarProps {
+  hasBorder?: boolean;
+  src: string;
+  alt?: string;
+}
+
+export function Avatar({ src, hasBorder = true, alt }: AvatarProps) {
   return (
     <img
       className={hasBorder ? styles.avatarWithBorder : styles.avatar}
       src={src}
+      alt={alt}
     />
   );
 }
