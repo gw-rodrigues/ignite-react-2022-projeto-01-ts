948b23034bdec83c236c019b3b63d9f02fcd7afc
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-10-19 17:49:30 +0200

    feat: add image extended props and rest operator

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,17 +1,15 @@
+import { ImgHTMLAttributes } from 'react';
 import styles from './Avatar.module.css';
 
-interface AvatarProps {
+interface AvatarProps extends ImgHTMLAttributes<HTMLImageElement> {
   hasBorder?: boolean;
-  src: string;
-  alt?: string;
 }
 
-export function Avatar({ src, hasBorder = true, alt }: AvatarProps) {
+export function Avatar({ hasBorder = true, ...rest }: AvatarProps) {
   return (
     <img
       className={hasBorder ? styles.avatarWithBorder : styles.avatar}
-      src={src}
-      alt={alt}
+      {...rest}
     />
   );
 }
